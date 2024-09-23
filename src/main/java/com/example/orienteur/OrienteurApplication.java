476a2db59d70f344cf298342357b833fcbeafc30
malakhbiyel <malak_hbiyel@outlook.fr>
2024-09-23 17:04:28 +0200

    first commit

---
+++ src/main/java/com/example/orienteur/OrienteurApplication.java
@@ -0,0 +1,13 @@
+package com.example.orienteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrienteurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrienteurApplication.class, args);
+    }
+
+}
